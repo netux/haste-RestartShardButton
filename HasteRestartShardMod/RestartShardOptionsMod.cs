c9cc7e72d6bb35e4b47ea0c58aa91b929709ff23
Martín (Netux) Rodriguez <send.email@netux.site>
2025-05-23 00:23:27 -0300

    feat: reuse Restart button from Haste 1.3.b, replace its handler
    Removes half of the code, because we don't need it anymore!

---
+++ HasteRestartShardMod/RestartShardOptionsMod.cs
@@ -0,0 +1,49 @@
+using Landfall.Modding;
+using Zorro.UI.Modal;
+
+namespace HasteRestartShardMod;
+
+[LandfallPlugin]
+public class RestartShardOptionsMod
+{
+    static RestartShardOptionsMod()
+    {
+        On.EscapeMenuMainPage.OnPageEnter += static (original, escapeMenuMainPage) =>
+        {
+            original(escapeMenuMainPage);
+
+            escapeMenuMainPage.m_QuickRestart.onClick.RemoveAllListeners();
+            escapeMenuMainPage.m_QuickRestart.onClick.AddListener(OpenRestartShardPrompt);
+        };
+    }
+
+    static void OpenRestartShardPrompt()
+    {
+        Modal.OpenModal(
+            new DefaultHeaderModalOption(
+                "Restart Run",
+                string.Join("\n", [
+                    "You will lose all progress from this run",
+                    "How would you like to restart?"
+                ]
+            )),
+            new ModalButtonsOption([
+                new(text: "New seed", () => RestartRun(keepSeed: false)),
+                new(text: "Same seed", () => RestartRun(keepSeed: true)),
+                new(text: "Cancel", () => { /* No-op. Modal already closes itself for us :^) */ })
+            ])
+        );
+    }
+
+    private static void RestartRun(bool keepSeed)
+    {
+        var config = RunHandler.config;
+        var shardID = RunHandler.RunData.shardID;
+        var seed = keepSeed
+            ? RunHandler.RunData.currentSeed
+            : RunHandler.GenerateSeed();
+
+        RunHandler.ClearCurrentRun();
+        RunHandler.StartAndPlayNewRun(config, shardID, seed);
+    }
+}
